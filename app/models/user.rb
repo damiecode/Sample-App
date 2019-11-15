e78878b9a21d5156da4ae99b179dc85dfba6f8d2
Ale Damilola <damidigi@yahoo.com>
2019-11-15 11:58:49 +0100

    validation of length and format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  
-  
+  validates :name, presence: true, length: { minimum:3, maximum:50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { minimum:3, maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
