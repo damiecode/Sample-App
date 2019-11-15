23fda43dd63e387757b5e969c35c914a9cbb4dbe
Ale Damilola <damidigi@yahoo.com>
2019-11-15 12:02:23 +0100

    duplicate validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { minimum:3, maximum:50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { minimum:3, maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
