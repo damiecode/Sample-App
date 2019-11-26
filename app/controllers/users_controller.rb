d101de4c7cd7fd34d5d239c6ea93f21d16362c81
damiecode <damidigi@yahoo.com>
2019-11-26 19:03:00 +0100

    Add password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   end
 
   def show
-      @user = User.find(params[:id])
-      redirect_to root_url and return unless @user.activated
+    @user = User.find(params[:id])
+    redirect_to(root_url) && return unless @user.activated
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
-  before_save   :downcase_email
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
@@ -52,8 +52,22 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   private
+
   # Converts email to all lower-case.
   def downcase_email
     self.email = email.downcase
